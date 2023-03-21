6edd811dc67e542222c99951bc660132d4101efc
tcrawley2 <62876986+tcrawley2@users.noreply.github.com>
2023-03-21 12:49:40 -0700

    Update settings.py
    allowed hosting in settings

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-+-g#yundpcw-e=unzg1-bb$h35uxy1a*2*-ts-@=q38)3u&hs+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
